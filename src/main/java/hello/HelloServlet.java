09d5e5c460df3d0f65f33b83af68a73da2f47382
haohong725 <haohong725@qq.com>
2018-02-03 19:44:45 +0800

    使用注解形式标注servlet 路由

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -1,11 +1,13 @@
 package hello;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/hello-servlet")
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
