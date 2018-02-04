7c57fd5eb5126e536ab6354b1bc910275b308613
haohong725 <haohong725@qq.com>
2018-02-04 22:58:10 +0800

    amend addClass method

--- src/main/java/com/walle/framework/util/ClassUtil.java
+++ src/main/java/com/walle/framework/util/ClassUtil.java
@@ -88,10 +88,10 @@ public class ClassUtil {
     }
 
     private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
-        File[] files = new File(packageName).listFiles(new FileFilter() {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
             @Override
             public boolean accept(File file) {
-                return (file.isFile() && file.getName().endsWith(".class") || file.isDirectory());
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
             }
         });
 
